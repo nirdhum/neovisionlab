d3aed42254578d8a0c04f5c88a771524cbe15541
Nirdhum <66917558+nirdhum@users.noreply.github.com>
2023-07-06 01:18:24 +0530

    Testimonials section

--- js/script.js
+++ js/script.js
@@ -57,4 +57,38 @@ const swiper = new Swiper(".team-card", {
     },
 })
 
+// Swiper
+const testimonialsSwiper = new Swiper(".testimonials", {
+    slidesPerView: 1,
+    spaceBetween: 5,
+    loop: false,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 1,
+        },
+        950: {
+            slidesPerView: 1,
+        },
+        1280: {
+            slidesPerView: 1,
+        }
+    },
+})
+
 
